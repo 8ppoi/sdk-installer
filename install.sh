e707cd6c122209161c3348f1ea5d66f870a29f89
Kenta Saito <kenta.saito@sagittar.org>
2026-01-28 13:24:08 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/8ppoi/sdk.git
 cd sdk
 ./bin/ensure-consoles
-./bin/clone-vendors 8ppoi
-./bin/clone-cartridges 8ppoi/invader-x
+./bin/download-vendors 8ppoi
+./bin/download-cartridges 8ppoi/invader-x
 ./bin/run-sdk
